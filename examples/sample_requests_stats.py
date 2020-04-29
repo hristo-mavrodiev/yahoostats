2f226e7f34e67d8c3419a5676012812257d50224
hristo-mavrodiev <h.mavrodiev@abv.bg>
2020-04-30 00:09:50 +0300

    Add yahoo_stocklist_stats reuters

--- examples/sample_requests_stats.py
+++ examples/sample_requests_stats.py
@@ -11,50 +11,96 @@ import requests
 from bs4 import BeautifulSoup as soup
 from time import sleep
 import pickle
+from pprint import pprint as pp
+# import locale
 
 
 proxies = {}
 ticker = 'GOOGL'
+# locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+# print(locale.getlocale(0))
+# locale.setlocale(locale.LC_ALL, 'en_US.utf8')
+# print(locale.getlocale())
 # cockie = {'enwiki_session': 'c1bc7dc8-0100-4fac-a608-08a1b6e5f4ed'}
 # cookie = pickle.load(open("cookies.pkl", "rb"))
 
 
-def yf_stats(ticker, cockie, proxies=None,):
+def get_page_content(url):
+    s = requests.Session()
+    # s.post(url, cockie)
+    res = s.get(url, headers={"User-Agent": "Mozilla/5.0"})
+    # print(s.cookies.get_dict())
+    if res.status_code == requests.codes['ok']:
+        soup1 = soup(res.content, "lxml")
+        return soup1
+    else:
+        return None
+
+
+def yf_stats(ticker, cockie=None, proxies=None,):
     """Function to webscrape stock statistics from yahoo finance.
     ticker: string ticker
     proxies: dictionary with proxy
     """
-    s = requests.Session()
-    url = "https://finance.yahoo.com/quote/GOOGL/key-statistics?p=GOOGL"
-    # post = s.post(url, cockie)
-    res = s.get(url, headers={"User-Agent": "Mozilla/5.0"})
-    if res.status_code == requests.codes.ok:
-        soup1 = soup(res.content, "lxml")
-        # print(soup1)
-        col = []
-        for header in soup1.findAll("th"):
-            col.append(header.text)
-        print(col)
-        try:
-            # assert(len(col) == 5)
-            df = pd.DataFrame(columns=col)
-            for row in soup1.findAll("tr"):
-                cells = row.findAll("td")
+
+    # url = "https://finance.yahoo.com/quote/GOOGL/key-statistics?p=GOOGL"
+    url = f"https://finance.yahoo.com/quote/{ticker}/key-statistics?p={ticker}"
+    soup1 = get_page_content(url)
+    # print(soup1)
+    col = []
+    for header in soup1.findAll("th"):
+        col.append(header.text)
+    print(col)
+    try:
+        # assert(len(col) == 5)
+        df = pd.DataFrame(columns=col)
+        for row in soup1.findAll("tr"):
+            cells = row.findAll("td")
+            row = []
+            # print(cells)
+            # Pos(st) Start(0) Bgc($lv2BgColor) fi-row:h_Bgc($hoverBgColor) Pend(10px)
+            if len(cells) > 1:
+                print(cells[0].text, end='')
+                print(" :", cells[1].text)
+                # print(" = ", cells[2].find(text=True))
+                # print(cells[3].find(text=True), end='')
+                # print(cells[4].find(text=True), end='')
+                # print(cells[5].find(text=True))
+    except Exception as exe:
+        print(exe)
+        print(f'Wetsite {url} changed need to edit the function.')
+        df = pd.DataFrame()
+    return df
+
+
+def reuters_stats(ticker, exchange):
+    """
+    Function to get data from Thompson Reuters"
+    https://www.reuters.com/finance/stocks/lookup?searchType=any&comSortBy=marketcap&search="COMPANY"
+    https://www.reuters.com/companies/GOOGL.O/key-metrics
+    """
+
+    url = f"https://www.reuters.com/companies/{ticker}.{exchange}/key-metrics"
+    soup1 = get_page_content(url)
+    try:
+        df = {}
+        df.update({ticker: {}})
+        for table in soup1.findAll('div', {'class': "KeyMetrics-table-container-3wVZN"}):
+            for h3 in table.findAll('h3'):
+                print(h3.text)
+            for row in table.findAll("tr"):
+                keys = row.findAll('th')
+                values = row.findAll("td")
                 row = []
-                # print(cells)
-                # Pos(st) Start(0) Bgc($lv2BgColor) fi-row:h_Bgc($hoverBgColor) Pend(10px)
-                if len(cells) > 1:
-                    print(cells[0].text, end='')
-                    print(" :", cells[1])
-                    # print(" = ", cells[2].find(text=True))
-                    # print(cells[3].find(text=True), end='')
-                    # print(cells[4].find(text=True), end='')
-                    # print(cells[5].find(text=True))
-        except Exception as exe:
-            print(exe)
-            print('Wetsite changed need to edit the function.')
-            df = pd.DataFrame()
-        return df
+                if keys and values:
+                    print({keys[0].text: values[0].text})
+                    df[ticker].update({keys[0].text: values[0].text})
+    except Exception as exe:
+        print(exe)
+        print('Wetsite {url} changed need to edit the function.')
+        df = pd.DataFrame()
+    return df
 
 
-yf_stats(ticker, cockie)
+# yf_stats(ticker)
+pp(reuters_stats(ticker, 'O'))
--- yahoostats/utils.py
+++ yahoostats/utils.py
@@ -27,6 +27,7 @@ class Webscraper:
             cockie_window = self.__driver.find_element_by_tag_name('body')
             cockie_window.find_element_by_name('agree').click()
             print('Cockies accepted.')
+            # pp(self.__driver.get_cookies())
         except Exception as exe:
             print('Unable to accept cockies.')
             print(exe)
@@ -34,7 +35,7 @@ class Webscraper:
     def stop(self):
         try:
             self.__driver.close()
-            print('Webscraper has finished.')
+            print('Webscraper has finished.Quit.')
         except Exception as exe:
             print('Unable to stop the Webscraper.')
             print(exe)
@@ -42,13 +43,26 @@ class Webscraper:
     def get_statistics(self, stock_list):
         stock_data = {}
         for stock in stock_list:
+            print(f'Start webscraping {stock}')
             stock_url = f"{self._url}/{stock}/key-statistics?p={stock}"
             self.__driver.get(stock_url)
-            soup = BeautifulSoup(self.__driver.page_source, "html.parser")
+            html = self.__driver.execute_script('return document.body.innerHTML;')
+            soup = BeautifulSoup(html, "html.parser")
+            stock_data.update({stock: {}})
             if "Symbols similar to" in soup.get_text():
                 print(f'The stock - {stock} was not found in Yahoo Finance.')
                 continue
-            stock_data.update({stock: {}})
+            else:
+                data = soup.find(id="Main")
+                tables = data.find_all('table')
+                for table in tables:
+                    rows = table.find_all('tr')
+                    # row_list = list()
+                    for tr in rows:
+                        td = tr.find_all('td')
+                        if len(td) > 1:
+                            stock_data[stock].update({td[0].text: td[1].text})
+
         return stock_data
 
     def scroll(self, px):
@@ -77,12 +91,15 @@ if __name__ == "__main__":
     firefox_options = Options()
     firefox_options.add_argument("--headless")
     firefox_options.add_argument('--no-sandbox')
-    ticker = 'GOOGL'
     url = f'https://finance.yahoo.com/quote'
     path_to_geckodriver = '/usr/local/bin'
-    stock_list = ['GOOGL', 'GTT1', 'VMW']
+    stock_list = ['GOOGL', 'GTT', 'VMW']
     yh = Webscraper(url, path_to_geckodriver, firefox_options)
     yh.start()
     yh.accept_cockies()
-    print(yh.get_statistics(stock_list))
+    result = yh.get_statistics(stock_list)
+    pp(result)
+    for i in stock_list:
+        peg_stock_0 = result[i].get('PEG Ratio (5 yr expected) 1')
+        print(f"PEG Ratio of {i} is {peg_stock_0}")
     yh.stop()
